3952dd78e4ec212fc0fa7a6769ab9c3d1724e9f0
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2022-09-01 21:08:12 +0800

    Hardhat Network Config

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${process.env.MATICVIGIL_PROJECT_ID}`,
+      account: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
+    },
+    mainnet: {
+      url: `https://rpc-mainnet.maticvigil.com/v1/${process.env.MATICVIGIL_PROJECT_ID}`,
+      account: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
+    },
+  },
 };
