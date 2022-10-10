6ac91c4ecefeea5e19a2b2abfe6c3e7bc0c17a74
Carl Justine Mosquida <38024229+mosquida@users.noreply.github.com>
2022-10-10 20:52:38 +0800

    Fixed Account Deploy Issue

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -8,11 +8,11 @@ module.exports = {
     },
     mumbai: {
       url: `https://rpc-mumbai.maticvigil.com/v1/${process.env.MATICVIGIL_PROJECT_ID}`,
-      account: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
+      accounts: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
     },
     mainnet: {
       url: `https://rpc-mainnet.maticvigil.com/v1/${process.env.MATICVIGIL_PROJECT_ID}`,
-      account: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
+      accounts: [process.env.MATIC_ACCOUNT_DEPLOYER_PRIV_KEY],
     },
   },
 };
